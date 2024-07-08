3fcc2df653e8115d2c660a39b02f80587eb9210b
Farouk-Code <farouk.sahraoui1@gmail.com>
2024-07-08 17:54:37 +0200

    Exercice : images avec react

---
+++ 01 - init/form-in-react/src/Form.jsx
@@ -0,0 +1,13 @@
+import { Component } from "react";
+
+class Form extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Commentaire</h1>
+      </div>
+    );
+  }
+}
+
+export default Form;
