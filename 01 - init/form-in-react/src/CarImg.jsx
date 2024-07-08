759df725905baf83bdf39d53ccf9305eefb67cad
Farouk-Code <farouk.sahraoui1@gmail.com>
2024-07-08 18:39:50 +0200

    Import image PNG en react

---
+++ 01 - init/form-in-react/src/CarImg.jsx
@@ -0,0 +1,7 @@
+import car from "./assets/car.png";
+
+const CarImg = () => {
+  return <img src={car} alt="Car" />;
+};
+
+export default CarImg;
--- 01 - init/form-in-react/src/Form.jsx
+++ 01 - init/form-in-react/src/Form.jsx
@@ -1,9 +1,11 @@
 import { Component } from "react";
+import CarImg from "./CarImg";
 
 class Form extends Component {
   render() {
     return (
       <div>
+        <CarImg />
         <h1>Commentaire</h1>
       </div>
     );
