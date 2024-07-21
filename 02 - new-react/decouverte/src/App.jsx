b112a8406c556ae2726be8fd101a0e2c578b2554
Farouk-Code <farouk.sahraoui1@gmail.com>
2024-07-21 23:17:57 +0200

    Test JSX

--- 02 - new-react/decouverte/src/App.jsx
+++ 02 - new-react/decouverte/src/App.jsx
@@ -1,5 +1,32 @@
+let hidden = false;
+const todos = ["Présenter react", "Presenter le JSX", "Créer des composants"];
+
 function App() {
-  return "Bonjour";
+  return (
+    <>
+      <Title color="green" hidden={hidden}>
+        Mon comoposant
+      </Title>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda, minus rerum, ducimus quidem atque, sed
+        dolore sequi rem consequatur doloremque labore. Ipsum ipsa veritatis distinctio beatae, ullam nesciunt optio
+        vero.
+      </p>
+
+      {todos.map((todo, index) => (
+        <li key={index}>{todo}</li>
+      ))}
+    </>
+  );
+}
+
+// eslint-disable-next-line react/prop-types
+function Title({ color, children, hidden }) {
+  if (hidden) {
+    return null;
+  }
+
+  return <h1 style={{ color: color }}>{children}</h1>;
 }
 
 export default App;
